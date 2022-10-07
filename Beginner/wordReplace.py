7769029f0e005f963c0c39e513e35d71c05bff99
Ayotunde Adetunji <adetunji1099@gmail.com>
2022-10-07 14:25:13 +0100

    Added another word to wordReplace

--- Beginner/wordReplace.py
+++ Beginner/wordReplace.py
@@ -1,7 +1,7 @@
 def replaceWord():
     #A funtion to replace a word in a sentence(string) with another
 
-    str = 'I want to replace these word word word'
+    str = 'I want to replace these word word word word'
     to_replace = input('Enter the word to be replaced: ')
     replacement = input('Enter the replacement word: ')
     print(str.replace(to_replace,replacement))
